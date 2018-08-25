4244e4353b142c2c8de9ec9782d231debdbd4e19
qtests <qtests@outlook.com>
2018-08-25 19:50:54 +0200

    Connecting TS Download - Version 1

--- src/Main.hs
+++ src/Main.hs
@@ -6,22 +6,24 @@ module Main where
 -- import Control.Concurrent.STM
 -- import Data.IntMap
 import Yesod
-import Database.Persist.Sql
-import Data.Pool
+import Database.Persist.Sql (ConnectionPool, SqlBackend, runSqlPool, runMigration)
+import Data.Pool (Pool(..))
 
 import Data.Text (pack)
-import Data.Time
-
+import Data.Time (UTCTime(..), fromGregorian, getCurrentTime)
+import Data.List (transpose, zipWith5, intercalate)
+import Data.Maybe (fromJust, isJust)
+ 
 import Control.Monad
-import Control.Monad.Logger
-import Control.Monad.Trans.Resource
-import Control.Monad.Trans.Reader
-import Control.Concurrent
+import Control.Monad.Logger (LoggingT, runStderrLoggingT)
+import Control.Monad.Trans.Resource (ResourceT, runResourceT)
+import Control.Monad.Trans.Reader (ReaderT)
+import Control.Concurrent (threadDelay, forkIO)
 
 -- https://hackage.haskell.org/package/bytestring-0.10.8.2/docs/Data-ByteString.html
 import qualified Data.ByteString as S
 
-import Dispatch
+import Dispatch ()
 import Foundation
 import Config
 import Model
@@ -35,17 +37,13 @@ import System.ReadEnvVar (readEnvDef)
 -- https://github.com/agrafix/users
 -- https://github.com/Daiver/HBlog
 
+
 dbFunction :: ReaderT SqlBackend (LoggingT (ResourceT IO)) a -> Pool SqlBackend  -> IO a
 dbFunction query pool = runResourceT $ runStderrLoggingT $ runSqlPool query pool
 
 
-doMigrations :: ReaderT SqlBackend (LoggingT (ResourceT IO)) ()
-doMigrations = do 
-    runMigration migrateAll
-
-
-doDbStuff :: ReaderT SqlBackend (LoggingT (ResourceT IO)) ()
-doDbStuff = do
+buildDb :: ReaderT SqlBackend (LoggingT (ResourceT IO)) ( )
+buildDb = do
 
     maybeIBM <- getBy $ UniqueTicker "IBM" True
     case maybeIBM of
@@ -54,7 +52,18 @@ doDbStuff = do
                       insert_ $ Company  "IBM Inc" "www.ibm.com" "IBM" True
                       return ()
         Just (Entity companyId cpny) -> liftIO $ print cpny
- 
+
+    maybeMSFT <- getBy $ UniqueTicker "MSFT" True
+    case maybeMSFT of
+        Nothing -> do
+                      liftIO $ putStrLn "Just kidding, Microsoft is not really there !"
+                      insert_ $ Company  "Microsoft Inc" "www.microsoft.com" "MSFT" True
+                      return ()
+        Just (Entity companyId cpny) -> liftIO $ print cpny
+
+-- *********************************************************************************************** -- 
+-- Time Series DB Stuff
+-- *********************************************************************************************** -- 
 
 addTextFile2DB :: String -> String -> ReaderT SqlBackend (LoggingT (ResourceT IO)) ()
 addTextFile2DB fileName filePath = do
@@ -63,18 +72,89 @@ addTextFile2DB fileName filePath = do
     insert_ $ StoredFile (pack fileName) "text/plain" fileContents time
 
 
+sendTS2DB :: String -> Either String [(UTCTime, [Double])] -> ReaderT SqlBackend (LoggingT (ResourceT IO)) ()
+sendTS2DB _ (Right []) = return ()
+sendTS2DB ticker timeSeries = 
+    case timeSeries of 
+        Left _    -> return ()
+        Right ts  -> do 
+                        maybeCpny <- getBy $ UniqueTicker (pack ticker) True
+                        if (isJust maybeCpny) 
+                            then
+                                do 
+                                    let (Entity companyId cpny) = fromJust maybeCpny
+                                    let (index, dta) = unzip ts
+
+                                    let [close, adjclose, volume] = transpose dta
+                                    let dbts = zipWith5 TimeSeries (repeat companyId) index close adjclose volume 
+                                    
+                                    -- Insert data
+                                    forM_ dbts insertUnique 
+
+                                    -- ids <- forM dbts insert
+                                    -- liftIO $ print ids
+                            else
+                                return ()
+
+
+sendTS2File :: String -> Either String [(UTCTime, [Double])] -> IO ()
+sendTS2File filePath timeSeries =
+    case timeSeries of 
+        Left _   -> writeFile filePath ""
+        Right ts -> if length ts > 0 then
+                        let (times, values) = unzip ts
+                            tsString = mconcat $ ["Date,Value\n"] ++ 
+                                    zipWith (\x y -> 
+                                        mconcat [show x,",", intercalate ", " $ fmap show y,"\n"] ) times values
+                        in writeFile filePath tsString
+                    else
+                        writeFile filePath ""
+
+
+queryIdTSSendEm2File :: String -> String -> ReaderT SqlBackend (LoggingT (ResourceT IO)) ()
+queryIdTSSendEm2File ticker filePath = do
+    maybeCpny <- getBy $ UniqueTicker (pack ticker) True
+    if (isJust maybeCpny) 
+        then
+            do 
+                let (Entity companyId cpny) = fromJust maybeCpny
+                tsRecords <- selectList [TimeSeriesTsid ==. companyId ] [ Asc TimeSeriesRefdate ]
+                
+                -- http://www.jakubkonka.com/2014/01/23/conduit-haskell.html
+                -- Check !!
+                let ts = fmap (\(Entity _ (TimeSeries _ refDate close adjclose vol)) -> 
+                                                    (refDate, [close, adjclose, vol]) ) tsRecords
+
+                liftIO $ sendTS2File filePath (Right ts)
+                -- liftIO $ print $ take 5 ts
+                -- return ()
+        else
+            liftIO $ sendTS2File filePath (Right [])
+
+
+-- All batch job
 tsDownloadJob :: [String] -> Int -> ConnectionPool -> IO ()
 tsDownloadJob tickers timeDelay conpool = 
     forever $ do
-        print "TS Download Job: Downloading data!"
-        downloadH2File tickers
+        print ("TS Download Job: Downloading data!" :: String)
 
-        let loadFile = addTextFile2DB "testFile_hd.csv" ""
-        dbFunction loadFile conpool 
+        -- Get the time series
+        let ticker = tickers !! 0
+        let  startDate = UTCTime (fromGregorian 2015 01 01) 0
+        ts <- priceTimeSeriesWithDate ticker startDate
+        
+        -- Save time series to file
+        -- sendTS2File "testFile_hts.csv" ts
+        -- dbFunction (addTextFile2DB "testFile_hts.csv" "") conpool 
 
-        -- Add time series to data base
+        -- Save to database
+        dbFunction (sendTS2DB ticker ts) conpool 
 
-        print "TS Download Job: Going to Sleep!"
+        -- Query company's Id, time series and save the later two file
+        dbFunction (queryIdTSSendEm2File ticker "testFile_htsdb.csv") conpool
+        dbFunction (addTextFile2DB "testFile_htsdb.csv" "") conpool 
+
+        print ("TS Download Job: Going to Sleep!" :: String)
         threadDelay timeDelay
 
 
@@ -85,12 +165,12 @@ main = do
 
     pool <- createPoolConfig persistConfig
 
-    dbFunction doMigrations pool
-    dbFunction doDbStuff pool 
+    dbFunction (runMigration migrateAll) pool
+    dbFunction buildDb pool 
 
     -- Download price time series
     -- let sleepTime = (10^6 * 60 * 5) :: Int
-    let sleepTime = (10^6 * 3600 * 4) :: Int
+    let sleepTime = (10^6 * 3600 * 6) :: Int
     _ <- forkIO $ tsDownloadJob ["IBM"] sleepTime pool
 
     -- Initialize the filestore to an empty map.
